c8d9299b60db0a99743907472a252342d5dc098a
wijden mediouni <148388774+wijdenmediouni1@users.noreply.github.com>
2025-10-09 22:13:00 +0100

    Update DepartmentController.java

--- src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
+++ src/main/java/tn/esprit/studentmanagement/controllers/DepartmentController.java
@@ -10,7 +10,7 @@ import tn.esprit.studentmanagement.services.IDepartmentService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/depatment")
+@RequestMapping("/department")
 @CrossOrigin(origins = "http://localhost:4200")
 @AllArgsConstructor
 public class DepartmentController {
